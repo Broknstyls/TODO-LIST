fdd3a3487506709e453403c5dcb965a92f7d924f
Broknstyls <muban90@gmail.com>
2023-11-04 14:00:48 +0200

    added light and dark theme stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+let fun =document.getElementById('theme');
+fun.onclick = function(){
+    if(document.getElementById('whole').style.background == 'rgb(26, 28, 28)'){
+        document.getElementById('whole').style.background = 'aliceblue';
+    } else{
+        document.getElementById('whole').style.background = 'rgb(26, 28, 28)'
+    }
+}
