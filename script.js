7e0697493b5be8c739cd7d7295e5a0b3d357d607
Broknstyls <muban90@gmail.com>
2023-11-05 08:59:19 +0200

    tried stuff???

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ fun.onclick = function(){
         document.getElementById('whole').style.background = 'rgb(26, 28, 28)'
     }
 }
+let add= document.getElementById('add').onclick = function(){
+let div = document.createElement('div');
+let todo = document.getElementById('objective').textContent;
+div.append = (todo);
+body.append(div);
+}
