23abc5ecce35352ebc0118d6780e9e37ed28f5b2
Broknstyls <muban90@gmail.com>
2023-11-13 07:19:35 +0200

    fixed light and dark mode button

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.querySelectorAll('span').onclick = function(){
     let span = document.querySelector('span');
     span.style.backgroundColor = 'rgb(67, 84, 95)';
 }
+let theme = document.getElementById('theme');
+theme.onclick = function(){
+    let color = document.getElementById('whole');
+    if(color.style.backgroundColor == 'rgb(26, 28, 28)'){
+        document.getElementById('theme').innerHTML = 'light mode'
+        color.style.backgroundColor = 'aliceblue';
+    } else{
+        color.style.backgroundColor = 'rgb(26, 28, 28)'
+        document.getElementById('theme').innerHTML = 'dark mode'
+
+
+    }
+}
