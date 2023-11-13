a5e7f6998eb12ee6bce01f47d8995393c9303b09
Broknstyls <muban90@gmail.com>
2023-11-13 06:57:57 +0200

    added hover effect

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ add.onclick = function(){
     span.textContent = document.getElementById('objective').value;
     document.body.append(span);
 }
+document.querySelectorAll('span').onclick = function(){
+    let span = document.querySelector('span');
+    span.style.backgroundColor = 'rgb(67, 84, 95)';
+}
